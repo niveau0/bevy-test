0f91961005ecf33d2536392f3b6abb3d2cd4635d
niveau0 <plingplong@t-online.de>
2025-08-09 12:00:16 +0200

    chore: update dependencies

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ const MINIMAP_SCREEN_PERCENTAGE: f32 = 20.0;
 #[derive(Component)]
 struct Zoom;
 
-fn zoom_config(screen_width: u32, screen_height: u32) -> (Viewport, OrthographicProjection) {
+fn zoom_config(screen_width: u32, screen_height: u32) -> (Viewport, Projection) {
     let scale = WORLD_SIZE as f32 / screen_width as f32 * 0.3;
     (
         Viewport {
@@ -23,16 +23,18 @@ fn zoom_config(screen_width: u32, screen_height: u32) -> (Viewport, Orthographic
             physical_size: UVec2::new(screen_width, screen_height),
             ..default()
         },
-        OrthographicProjection {
+        Projection::Orthographic(OrthographicProjection {
             near: 0.0,
             far: 1500.0,
+            viewport_origin: Default::default(),
+            scaling_mode: Default::default(),
             scale,
-            ..default()
-        },
+            area: Default::default(),
+        }),
     )
 }
 
-fn minimap_config(screen_width: u32, screen_height: u32) -> (Viewport, OrthographicProjection) {
+fn minimap_config(screen_width: u32, _screen_height: u32) -> (Viewport, Projection) {
     let map_screen_size = (screen_width as f32 * MINIMAP_SCREEN_PERCENTAGE / 100.0) as u32;
     (
         Viewport {
@@ -40,12 +42,14 @@ fn minimap_config(screen_width: u32, screen_height: u32) -> (Viewport, Orthograp
             physical_size: UVec2::new(map_screen_size, map_screen_size),
             ..default()
         },
-        OrthographicProjection {
+        Projection::Orthographic(OrthographicProjection {
             near: 0.0,
             far: 1500.0,
+            viewport_origin: Default::default(),
+            scaling_mode: Default::default(),
             scale: WORLD_SIZE as f32 / map_screen_size as f32,
-            ..default()
-        },
+            area: Default::default(),
+        }),
     )
 }
 
@@ -56,25 +60,22 @@ fn setup(
 ) {
     let texture_handle: Handle<Image> = server.load("tex1.png");
     let texture_handle2: Handle<Image> = server.load("tex2.png");
-    let window = q_window.single();
+    let window = q_window.single().unwrap();
 
     let (viewport, projection) = zoom_config(
         window.resolution.physical_width(),
         window.resolution.physical_height(),
     );
     commands.spawn((
-        Camera2dBundle {
-            camera: Camera {
-                is_active: true,
-                order: 1,
-                viewport: Some(viewport),
-                ..default()
-            },
-            transform: Transform::from_translation(Vec3::ZERO.with_z(100.0)),
-            projection,
-            camera_2d: Camera2d {},
+        Camera2d,
+        Camera {
+            is_active: true,
+            order: 1,
+            viewport: Some(viewport),
             ..default()
         },
+        Transform::from_translation(Vec3::ZERO.with_z(100.0)),
+        projection,
         RenderLayers::layer(RENDER_LAYER_ZOOM),
         Name::new("Zoom Camera"),
         Zoom,
@@ -85,18 +86,15 @@ fn setup(
         window.resolution.physical_height(),
     );
     commands.spawn((
-        Camera2dBundle {
-            camera: Camera {
-                is_active: true,
-                order: 2,
-                viewport: Some(viewport),
-                ..default()
-            },
-            transform: Transform::from_translation(Vec3::ZERO.with_z(200.0)),
-            projection,
-            camera_2d: Camera2d {},
+        Camera2d,
+        Camera {
+            is_active: true,
+            order: 2,
+            viewport: Some(viewport),
             ..default()
         },
+        Transform::from_translation(Vec3::ZERO.with_z(200.0)),
+        projection,
         RenderLayers::layer(RENDER_LAYER_MINIMAP),
         Name::new("Minimap Camera"),
     ));
@@ -108,7 +106,7 @@ fn setup(
         (0..10).for_each(|col| {
             new_sprite(
                 &mut commands,
-                if rnd.gen::<u8>() % 2 == 0 {
+                if rnd.random::<u8>() % 2 == 0 {
                     &texture_handle
                 } else {
                     &texture_handle2
@@ -124,18 +122,17 @@ fn new_sprite(commands: &mut Commands, texture_handle: &Handle<Image>, x: f32, y
     let scale = 800f32 * 2f32 / 512f32;
 
     commands.spawn((
-        SpriteBundle {
-            sprite: Sprite { ..default() },
-            texture: texture_handle.clone(),
-            visibility: Visibility::Visible,
-            transform: Transform::from_scale(Vec3 {
-                x: scale,
-                y: scale,
-                z: 1f32,
-            })
-                .with_translation(Vec3::new(x, y, -280.0)),
+        Visibility::Visible,
+        Sprite {
+            image: texture_handle.clone(),
             ..default()
         },
+        Transform::from_scale(Vec3 {
+            x: scale,
+            y: scale,
+            z: 1f32,
+        })
+        .with_translation(Vec3::new(x, y, -280.0)),
         RenderLayers::from_layers(&[RENDER_LAYER_ZOOM, RENDER_LAYER_MINIMAP]),
     ));
 }
@@ -158,7 +155,7 @@ fn main() {
 }
 
 fn control(input: Res<ButtonInput<KeyCode>>, mut q_transform: Query<&mut Transform, With<Zoom>>) {
-    let mut transform = q_transform.single_mut();
+    let mut transform = q_transform.single_mut().unwrap();
     let step = 20.0;
     for key in input.get_pressed() {
         match key {
@@ -183,11 +180,7 @@ fn control(input: Res<ButtonInput<KeyCode>>, mut q_transform: Query<&mut Transfo
                 );
             }
             KeyCode::KeyB => {
-                *transform = Transform::from_translation(Vec3::new(
-                    13120.0,
-                    2020.0,
-                    100.0,
-                ));
+                *transform = Transform::from_translation(Vec3::new(13120.0, 2020.0, 100.0));
             }
             _ => (),
         }
